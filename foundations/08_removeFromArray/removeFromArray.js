a28b16cec8b8f685cf9b9852bcd9216773e701e2
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-13 14:17:37 +0200

    Complete 08_removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function(array, remove) {
-    return array.filter((element) => element !== remove);
+const removeFromArray = function(array, ...itemsToRemove) {
+    for (const arg of itemsToRemove) {
+        array = array.filter((element) => element !== arg);
+    }
+    return array;
+
+    // Alternative direction (short form)
+    // return array.filter((element) => !itemsToRemove.includes(element));
 };
 
 // Do not edit below this line
