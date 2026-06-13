08ddb5ccfcebb395afd96f69952398ae810f0637
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-13 14:37:27 +0200

    Complete 09_sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(boundary1, boundary2) {
+    if (boundary1 < 0 || boundary2 < 0 || !Number.isInteger(boundary1) || !Number.isInteger(boundary2)) {
+        return "ERROR";
+    }
+    let result = 0;
+    const lowerBound = Math.min(boundary1, boundary2);
+    const upperBound = Math.max(boundary1, boundary2);
+    for (let i = lowerBound; i <= upperBound; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
