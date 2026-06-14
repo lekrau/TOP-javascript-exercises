c1fb2cf7e5f8d1f01f499fc15ff362cf625aa697
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-14 08:00:07 +0200

    Complete 11_tempConversion exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemperature) {
+  const exactConversion = (fahrenheitTemperature - 32) * 5 / 9;
+  return Math.round(exactConversion * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) {
+  const exactConversion = celsiusTemperature * 9 / 5 + 32;
+  return Math.round(exactConversion * 10) / 10;
 };
 
 // Do not edit below this line
