c66b22304dfff133e8e3324e4d490426ede9b798
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-14 07:48:40 +0200

    Complete 10_leapYears exercise

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400 === 0) {
+        return true;
+    }
+    if (year % 4 === 0 && year % 100 !== 0) {
+        // Should I write this else? It has no effect
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
