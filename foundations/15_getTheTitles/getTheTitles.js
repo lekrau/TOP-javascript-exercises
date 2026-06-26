f9f749d7499bd2a7d0fc8aa9ff3457878b91b83d
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-26 11:24:26 +0200

    Complete 15_getTheTitles exercise

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookArray) {
+    // const titleArray = bookArray.reduce((result, book) => {
+    //     result.push(book.title);
+    //     return result;
+    // }, []);
+    const titleArray = bookArray.map(book => book.title);
+    return titleArray;
 };
 
 // Do not edit below this line
