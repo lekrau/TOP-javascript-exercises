2a1b3ab182250b8a9021045f52f399261db536f0
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-26 08:27:21 +0200

    Complete 13_palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const lowercase = string.toLowerCase();
+    const regExp = /[^a-z0-9]/g;
+    const clean = lowercase.replaceAll(regExp, "");
+    const reverse = reverseString(clean);
+    return reverse === clean;
 };
+
+const reverseString = string => {
+    const array = string.split("");
+    const reverseArray = array.reverse();
+    return reverseArray.join("");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
