072eda73dd29cfaa66b14e0ce429e8b1d458e6ce
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-26 15:13:01 +0200

    Use reduce to find oldest person
    Extract age calculation into a getAge helper.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,17 +1,24 @@
-const findTheOldest = function(peopleArray) {
-    const sortedArray = peopleArray.sort((a, b) => {
-        let aAge = a.yearOfDeath - a.yearOfBirth;
-        if (isNaN(aAge)) {
-            aAge = new Date().getFullYear() - a.yearOfBirth;
+const findTheOldest = function(people) {
+    const result = people.reduce((oldestPerson, currentPerson) => {
+        const ageCurrent = getAge(currentPerson);
+        const ageOldest = getAge(oldestPerson);
+        if (ageCurrent > ageOldest) {
+            return currentPerson;
+        } else {
+            return oldestPerson;
         }
-        let bAge = b.yearOfDeath - b.yearOfBirth;
-        if (isNaN(bAge)) {
-            bAge = new Date().getFullYear() - b.yearOfBirth;
-        } 
-        return aAge - bAge;
     });
-    const result = sortedArray[sortedArray.length - 1];
     return result;
+};
+
+const getAge = person => {
+    if (person.yearOfDeath === undefined) {
+        // Person is still alive
+        const currentYear = new Date().getFullYear();
+        return currentYear - person.yearOfBirth;
+    } else {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
 };
 
 // Do not edit below this line
