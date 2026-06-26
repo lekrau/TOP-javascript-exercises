6cac23d747388e30a507c1b1a222dc716c079963
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-26 11:57:32 +0200

    Complete 16_findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    const sortedArray = peopleArray.sort((a, b) => {
+        let aAge = a.yearOfDeath - a.yearOfBirth;
+        if (isNaN(aAge)) {
+            aAge = new Date().getFullYear() - a.yearOfBirth;
+        }
+        let bAge = b.yearOfDeath - b.yearOfBirth;
+        if (isNaN(bAge)) {
+            bAge = new Date().getFullYear() - b.yearOfBirth;
+        } 
+        return aAge - bAge;
+    });
+    const result = sortedArray[sortedArray.length - 1];
+    return result;
 };
 
 // Do not edit below this line
