77d6e788a658eafbc900b12c1d4fc3a2de0893ff
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-26 11:12:43 +0200

    Complete 14_fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(nthMember) {
+    if (nthMember < 0) {
+        return "OOPS";
+    }
+    const sequence = [0, 1];
+    for (let i = 1; i < nthMember; i++) {
+        sequence.push(sequence[i] + sequence[i - 1]);
+    }
+    return sequence[nthMember];
 };
 
 // Do not edit below this line
